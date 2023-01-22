f9739d13991f011aa5857ec2e6a7a56ac8d58bf2
gustavolopes2030 <123329292+gustavolopes2030@users.noreply.github.com>
2023-01-22 18:01:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+
+function alternarModal() {
+    modal.classList.toggle("aberto");
+}
+ 
+botaoTrailer.addEventListener("click", () => {
+    console.log("clicou no botão veja o trailer");
+    alternarModal();
+video.setAttribute("src", linkDoVideo);
+});
+
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");      
+});
+
+
+
+
+
